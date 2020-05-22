bab62233cea357311a02d4c1042320ba59964fb6
J. Cohen <hostelmarketing@gmail.com>
2020-05-22 12:56:28 -0700

    Fix outdated comment

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ flipTemperature("Spain", 100); // this will convert 100C to fahrenheit
  * Flip temperature from C to F or F to C depending on the country.
  */
 function flipTemperature(country, temperature) {
-    // This will get the scale for the US (e.g., "fahrenheit").
+    // This will get the scale for the country (e.g., "fahrenheit", "celsius").
     const scale = tools.countries[country];
 
     // if it's fahrenheit
     if (scale === tools.measurements.F) {
         console.log(`The flipped temperature for is ${tools.f2c(temperature)}`);
+    // else if it's celsius
     } else if (scale === tools.measurements.C) {
         console.log(`The converted temperature is ${tools.c2f(temperature)}`);
     } else {
